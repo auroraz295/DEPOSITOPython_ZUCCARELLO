8abda9e3f6f20d602e46276caca77d93e2fd6c3e
Aurora Zuccarello <aurorazuccarello295@gmail.com>
2026-04-27 19:10:47 +0200

    Add files via upload

--- lezione_11_27-04/lezione11_esercizi_polimorfismo.py
+++ lezione_11_27-04/lezione11_esercizi_polimorfismo.py
@@ -4,7 +4,7 @@
 #azione, permettendo alle diverse sottoclassi di implementare i loro specifici
 #comportamenti di pagamento, pur aderendo all'interfaccia comune definita dalla classe
 #base.
-import random 
+'''import random 
 
 #SUPERCLASSI
 class Utente:
@@ -73,7 +73,7 @@ match comando:
 
     case 4:
         gestione.paga(metodo_scelto3, importo1)
-
+'''
 
 #ESERCIZIO2 
 
@@ -85,32 +85,39 @@ class Posto:
         self._occupato = False 
         
     #PRENOTAZIONE
-    #se l'inizializzazione è false non si può prenotare, 
-    #altrimenti è prenotabile
+    #se l'inizializzazione è False non si può prenotare, 
+    #altrimenti è prenotabile e poi lo stato occupato diventa True
     def prenota(self):
-        if self._occupato == False:
+        if self._occupato == True:
             print("Ci dispiace, il posto non è prenotabile.")
         else:
+            self._occupato = True
             print(f"Il posto numero {self._numero} della fila {self._fila} è stato prenotato.")
     
     #se il posto è occupato cambia l'inizializzazione da false a true per renderlo libero
     def libera(self):
-        if self._occupato == False:
-            _occupato = True
+        if self._occupato == True:
+            self._occupato = False
             print(f"Il posto numero {self._numero} della fila {self._fila} è stato liberato.")
         else:
             print("Il posto non era stato prenotato. ")
       
     #restituisce informazioni su numero, fila e stato occupato      
-    def get_info(self):
-        return self._numero and self._fila and self._occupato 
+    def get_info_numero(self):
+        return self._numero
+    
+    def get_info_fila(self):
+        return self._fila 
+    
+    def get_info_occupato(self):
+        return self._occupato 
     
 #CLASSI DERIVATE
 
 #POSTO VIP
 class PostoVIP(Posto):
-    def __init__(self, servizi_extra:list):
-        super().__init__(self._numero, self._fila)
+    def __init__(self, numero, fila):
+        super().__init__(numero, fila)
         
         #lista di servizi extra in più
         servizi_extra = ["Accesso al lounge", "Servizio in posto", "Cena inclusa"]
@@ -118,75 +125,124 @@ class PostoVIP(Posto):
     
     #se il posto è occcupato non è prenotabile, altrimenti si possono scegliere dei servizi extra
     def prenota(self):
-        if self._occupato == False:
+        if self._occupato == True:
             print("Ci dispiace, il posto non è prenotabile.")
         else:
-            print(f"Il posto numero {self._numero} della fila {self._fila} è stato prenotato.")
+            self._occupato = True
+            print(f"Il posto VIP numero {self._numero} della fila {self._fila} è stato prenotato.")
+            
+            #menu per la scelta dei servizi_extra
             while True:
-                comando_posto_vip = int(input("Attivazione servizi extra: 1. Accesso al lounge 2. Servizio in posto 3. Cena inclusa"))
+                comando_posto_vip = int(input("Attivazione servizi extra: 1. Accesso al lounge 2. Servizio in posto 3. Cena inclusa - "))
                 match comando_posto_vip:
                     
-                    #SERVIZI EXTRA DA DEFINIRE
                     case 1:
-                        pass
+                        print(f"Servizio extra {self.__servizi_extra[0]} aggiunto alla prenotazione.")
+                        break
                     case 2:
-                        pass
+                        print(f"Servizio extra {self.__servizi_extra[1]} aggiunto alla prenotazione.")
+                        break
                     case 3:
-                        pass
+                        print(f"Servizio extra {self.__servizi_extra[2]} aggiunto alla prenotazione.")
+                        break
                     case _:
                         print("Opzione non disponibile.")
+                        break
     
-#POSTO STANDARD - DA DEFINIRE
+#POSTO STANDARD 
 class PostoStandard(Posto):
-    def __init__(self, costo):
-        super().__init__(self._numero, self._fila)
-        self.__costo = costo
+    def __init__(self, numero, fila):
+        super().__init__(numero, fila)
+        
+        self._costo = 7.50
     
     def prenota(self):
-        pass
-    
-#CLASSE TEATRO - DA DEFNIRE
+        if self._occupato == True:
+            print("Ci dispiace, il posto non è prenotabile.")
+        else:
+            #c'è un costo aggiuntivo, si chiede all'utente se si è sicuri di comprare il biglietto
+            comando_posto_standard = input(f"Comprare il biglietto online ha un costo aggiuntivo di {self._costo} euro. Procedere? SI / NO ")
+            match comando_posto_standard:
+                case "SI":
+                    self._occupato = True
+                    print(f"Il posto Standard numero {self._numero} della fila {self._fila} è stato prenotato.")
+                
+                case "NO":
+                    print("Tornare al menu principale.")
+
+#CLASSE TEATRO 
 class Teatro:
-    def __init__(self, posti:list):
+    def __init__(self):
+        posti = []
         self._posti = posti
         
-    def aggiungi_posto(self, posto):
-        pass
+    #aggiunge l'oggetto posto alla lista
+    def aggiungi_posto(self, posto:Posto):
+        self._posto = posto 
+        self._posti.append(posto)
     
+    #cerca il posto nella lista e chiama il metodo prenota
     def prenota_posto(self, numero, fila):
-        pass
+        for p in self._posti:
+            if p.get_info_numero() == numero and p.get_info_fila() == fila:
+                p.prenota()
+                break
+        else:
+            print("Il posto non esiste.")
+     
+    #cerca il posto nella lista e se lo trova chiama il metodo libera       
+    def libera_posto(self, numero, fila):
+        for p in self._posti:
+            if p.get_info_numero() == numero and p.get_info_fila() == fila:
+                p.libera()
+                break
+        else:
+            print("Il posto non esiste.")  
     
+    #per ogni oggetto nella lista, se lo stato è occupato allora stampa le informazioni
     def stampa_posti_occupati(self):
-        pass
-        
-    
+        trovato = False
+        for p in self._posti:
+            if p.get_info_occupato() == True:
+                print(f"Numero: {p.get_info_numero()} - Fila: {p.get_info_fila()}")
+                trovato = True
+        if trovato == False:
+            print("Non ci sono posti occupati.")
 
+#creo teatro
+prova_teatro = Teatro()
 
-# MAIN - DA DEFINIRE
+prova_teatro.aggiungi_posto(PostoStandard(1, "A"))
+prova_teatro.aggiungi_posto(PostoStandard(2, "A"))
+prova_teatro.aggiungi_posto(PostoVIP(10, "VIP"))
+
+# MAIN 
 while True:
     comando = input("Vuoi accedere al teatro? SI / NO - ")
     match comando: 
         case "SI":
-            comando2 = int(input("A quale servizio vuoi accedere? 1. Prenota posto 2. Libera posto - "))
+            comando2 = int(input("A quale servizio vuoi accedere? 1. Prenota posto 2. Libera posto 3. Stampa posti occupati - "))
             match comando2:
+                
+                #prenota posto
                 case 1:
-                    comando3 = int(input("1. Prenotazione Posto Standard 2. Prenotazione Posto VIP - "))
-                    match comando3:
-                        
-                        case 1:
-                            pass
-                        
-                        case 2:
-                            pass
-                        
+                    num = int(input("Inserisci il numero posto: "))
+                    fila = input("Inserisci la fila: ")
+                    prova_teatro.prenota_posto(num, fila)
+                
+                #libera posto        
                 case 2:
-                    pass
+                    num = int(input("Inserisci il numero posto: "))
+                    fila = input("Inserisci la fila: ")
+                    prova_teatro.libera_posto(num, fila)
+                
+                #stampa posti occupati
+                case 3:
+                    prova_teatro.stampa_posti_occupati()
                 
                 case _:
-                    print("Opzione non disponibile.")
-                
-                
-                
+                    print("Opzione non disponibile.")     
+                      
         case "NO":
             break
         
